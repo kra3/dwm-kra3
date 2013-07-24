595416935fc51d945f8c38473efd74cde8189fa5
Arun K. R. (kra3) <the1.arun@gmail.com>
2013-07-24 10:49:23 +0200

    Added few rules for firefox and gimp

--- config.def.h
+++ config.def.h
@@ -25,6 +25,8 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ NULL,     NULL,       NULL,       0,            False,        -1 },
+    { "Gimp",     NULL,       NULL,       0,            True,        -1 },
+    { "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
 };
 
 /* layout(s) */
