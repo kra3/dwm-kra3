99a56589886e5faa00b71ac58a6613cc9d0e6ead
Arun K. R. kra3 <the1.arun@gmail.com>
2013-08-07 21:17:59 +0200

    Added comments to all short cuts &  set shortcuts for both xterm and uxterm

--- config.def.h
+++ config.def.h
@@ -61,33 +61,35 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "uxterm", NULL };
+static const char *uxtermcmd[]  = { "uxterm", NULL };
+static const char *urxvtccmd[]  = { "urxvtc", NULL };
 static const char *i3lockcmd[]  = { "i3lock", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ControlMask,           XK_Return, spawn,          {.v = uxtermcmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = urxvtccmd } },
 	{ MODKEY|ControlMask|ShiftMask, XK_l,      spawn,          {.v = i3lockcmd } },
-
+    /* on/off the bar */
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-
+    /* rotate focus between windows */
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,             			XK_Left,   focusstack,     {.i = +1 } },
 	{ MODKEY,             			XK_Right,  focusstack,     {.i = -1 } },
-
+    /* rotate through tags - ie., change view to another tag */
 	{ MODKEY|ControlMask, 			XK_j,      shiftview,      {.i = -1 } },
 	{ MODKEY|ControlMask, 			XK_k,      shiftview,      {.i = +1 } },
 	{ MODKEY|ControlMask, 			XK_Left,   shiftview,      {.i = -1 } },
 	{ MODKEY|ControlMask, 			XK_Right,  shiftview,      {.i = +1 } },
-
+    /* supposedly adjust number of master area?? */
 	{ MODKEY|ControlMask,           XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY|ControlMask,           XK_d,      incnmaster,     {.i = -1 } },
-
+    /* adjust master area size */
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-
+    /* change layouts */
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_d,      setlayout,      {.v = &layouts[2]} },
@@ -95,21 +97,25 @@ static Key keys[] = {
 	{ MODKEY,                       XK_v,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[5]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
-
+    /* switch floating on/off for focused client */
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+    /* kill focused client */
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+    /* put focused client in master area */
 	{ MODKEY,                       XK_Return, zoom,           {0} },
+    /* Make current window available in all tags - sticky */
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-
+    /* show all clients in current tag */
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+    /* switch to previous tag */
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-
+    /* change focused monitor */
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-
+    /*  I forgot !! Doesn't have an extra monitor now a days :( */
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-
+    /* change to tag */
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -119,7 +125,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-
+    /* restart dwm? and quit dwm */
 	{ MODKEY|ShiftMask,             XK_r,      self_restart,   {0} },
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
@@ -129,15 +135,19 @@ static Key keys[] = {
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[3]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = urxvtccmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+    /* change tag */
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
+    /* show all clients in another tag simultaneously */
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+    /* move focused client to another tag */
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+    /* show focused client in another tag along too */
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
